04ea341536a52efe0bf0b4a9de002a1610daad44
Jhone-cmd <jhone.1941@gmail.com>
2026-01-27 15:11:45 -0300

    feat: user functionalities

---
+++ src/main/java/com/jhone_cmd/TodoList/users/UserController.java
@@ -0,0 +1,17 @@
+package com.jhone_cmd.TodoList.users;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@RestController()
+@RequestMapping("/users")
+public class UserController {
+
+    @PostMapping("")
+    public void createUser(@RequestBody UserModel userModel) {
+        System.out.println(userModel.name);
+    }
+
+}
---
+++ src/main/java/com/jhone_cmd/TodoList/users/UserModel.java
@@ -0,0 +1,8 @@
+package com.jhone_cmd.TodoList.users;
+
+public class UserModel {
+    private String id;
+    private String name;
+    private String email;
+    private String password;
+}
